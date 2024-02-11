81732c335d9fac4c196920b416222e6917e1759e
tvv91 <vladimirturovets@gmail.com>
2024-02-11 14:59:16 +0200

    Add project files.

---
+++ CustomMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Platform
+{
+    public class CustomMiddleware
+    {
+        private RequestDelegate _next;
+        public CustomMiddleware()
+        {
+            
+        }
+
+        public CustomMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+            {
+                await context.Response.WriteAsync("Class-based middlware \n");
+            }
+            if (_next != null)
+            {
+                await _next(context);
+            }
+        }
+    }
+}
---
+++ LocationMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+namespace Platform
+{
+    public class LocationMiddleware
+    {
+        private RequestDelegate _next;
+        private MessageOptions _options;
+        public LocationMiddleware(RequestDelegate next, IOptions<MessageOptions> options)
+        {
+            _next = next;
+            _options = options.Value;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path == "/location")
+            {
+                await context.Response.WriteAsync($"{_options.CityName}, {_options.CountryName}");
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+    }
+}
